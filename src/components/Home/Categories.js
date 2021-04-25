e5efabca3e5626036c3a6a0d282e9da748752734
Vári Róbert <mail.robertvari@gmail.com>
2021-04-25 14:20:17 +0200

    home

--- src/components/Home/Categories.js
+++ src/components/Home/Categories.js
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
+import {Link} from "react-router-dom";
+
+const API_URL = process.env.REACT_APP_API_URL
+
+function CategoryCard({data}){
+    return(
+        <Link to={`/categories/${data.title}`} className={"card"}>
+            {data.title}
+        </Link>
+    )
+}
+
 
 function Categories(props) {
+    const [categories, set_categories] = useState([])
+
+    const fetch = () => {
+        axios({
+            method: "get",
+            url: `${API_URL}/categories`
+        })
+            .then(res => set_categories(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
+
     return (
-        <div>Categories</div>
+        <div className="home-collection-container">
+            <h1>Shop For</h1>
+            <div className="cards-container">
+                {
+                    categories.map(data => <CategoryCard key={data.id} data={data}/>)
+                }
+            </div>
+        </div>
     );
 }
 
