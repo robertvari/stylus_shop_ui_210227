886508de8194fe44f6816807900c5af19ec977f3
Vári Róbert <mail.robertvari@gmail.com>
2021-04-25 14:56:53 +0200

    ItemListPage.js

--- src/components/Pages/ItemListPage.js
+++ src/components/Pages/ItemListPage.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router-dom"
+import axios from "axios";
+
+const API_URL = process.env.REACT_APP_API_URL
 
 function ItemListPage(props) {
     const {category} = useParams()
     const {subcategory} = useParams()
+    const [items, set_items] = useState([])
+
+    const fetch = () => {
+        axios({
+            method: "get",
+            url: `${API_URL}/items`
+        })
+            .then(res => set_items(res.data))
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
 
     return (
-        <div>
-            <h1>{category}</h1>
-            <h2>{subcategory}</h2>
+        <div className="item-list-container">
+            {
+                items.filter(
+                    data => subcategory?
+                        data.category === category && data.subcategory === subcategory
+                        :
+                        data.category === category
+                ).map(data => <div>{data.title}</div>)
+            }
         </div>
     );
 }
