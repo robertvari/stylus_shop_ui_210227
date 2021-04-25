807a3790e50aeadd608951579e7207e6ce02a688
Vári Róbert <mail.robertvari@gmail.com>
2021-04-25 15:19:46 +0200

    ItemListPage.js

--- src/components/Home/ItemCard.js
+++ src/components/Home/ItemCard.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function ItemCard({data}) {
     return (
-        <div className="card item-card">
+        <Link to={`/details/${data.id}`} className="card item-card">
             <img src={data.image} alt=""/>
             <small>{data.title}</small>
             <div className="price">{data.price}</div>
-        </div>
+        </Link>
     );
 }
 
--- src/components/Pages/ItemDetailsPage.js
+++ src/components/Pages/ItemDetailsPage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import {useParams} from "react-router-dom";
 
 function ItemDetailsPage(props) {
+    const {id} = useParams()
+
     return (
-        <h1>Details Page</h1>
+        <h1>Details Page: {id}</h1>
     );
 }
 
