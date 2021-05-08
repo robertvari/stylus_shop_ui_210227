2907d3755540d7c0d56d55c251e0880dcc0fbdd6
Robert Vári <mail.robertvari@gmail.com>
2021-05-08 16:50:02 +0200

    Registration page

--- src/components/Pages/RegistrationPage.js
+++ src/components/Pages/RegistrationPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function RegistrationPage(props) {
     return (
-        <h1>Registration</h1>
+        <div className="center-page-container">
+            <form>
+                <h1>CREATE ACCOUNT</h1>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+
+                <button>CREATE</button>
+
+                <small>or <Link to="/">Return to store</Link></small>
+            </form>
+        </div>
     );
 }
 
