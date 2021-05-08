42ed2f08a0b5d28bee3a97f41b32cb7ed09ec01b
Robert Vári <mail.robertvari@gmail.com>
2021-05-08 16:44:32 +0200

    Sign in page

--- src/components/Pages/PasswordResetPage.js
+++ src/components/Pages/PasswordResetPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function PasswordResetPage(props) {
     return (
-        <h1>Password Reset Page</h1>
+        <div className="center-page-container">
+            <form>
+                <h1>RESET YOUR PASSWORD</h1>
+                <small>We will send you an email to reset your password.</small>
+                <input type="email" placeholder="Email"/>
+
+                <button>SUBMIT</button>
+
+                <small>or <Link to="/">Return to store</Link></small>
+            </form>
+        </div>
     );
 }
 
--- src/components/Pages/SignInPage.js
+++ src/components/Pages/SignInPage.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 function SignInPage(props) {
     return (
-        <h1>Sign In</h1>
+        <div className="center-page-container">
+            <form>
+                <h1>Sign In</h1>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+
+                <Link to="/password-reset"><small>Forgot your password?</small></Link>
+
+                <button>Sign In</button>
+
+                <small>or <Link to="/">Return to store</Link></small>
+            </form>
+        </div>
     );
 }
 
